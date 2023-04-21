ba7d50d422116c23a54bddb3151415af95f55956
Raul772 <raulvictor772@gmail.com>
2023-04-21 15:59:50 -0300

    Simple Start of a new Project

---
+++ AppMvcBasica/AppMvcBasica/Data/Migrations/20230421182147_Atualizando.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+
+namespace AppMvcBasica.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Atualizando : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
